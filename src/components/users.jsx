b76c60d1d2f1b3a475ff9d12fdd3e535b0b4c3bf
Evgeniy Bazilov <jenya12d@gmail.com>
2022-12-16 21:41:47 +0500

    render table

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,45 +1,97 @@
-import React, { useState } from "react";
+import React, { useState, useTransition } from "react";
 import API from "../API";
+import "../index.css";
 
 const Users = () => {
   const [users, setUsers] = useState(API.users.fetchAll());
+  const [phrases, setPhrases] = useState("человек тусанёт с тобой сегодня");
 
-  //console.log("users ", users);
+  //console.log(phrase);
+
+  const ph = [
+    "человек тусанёт с тобой сегодня",
+    "человека тусанёт с тобой сегодня",
+    "Никто с тобой не тусанёт",
+  ];
+
+  let number = users.length;
+  let phrase = phrases;
 
   const handleDelete = (userId) => {
-    console.log(users);
+    setUsers(users.filter((p) => p._id !== userId));
+    renderPhrase(number - 1);
   };
-  const renderPhrase = (namber) => {
-    console.log("zzzz");
+
+  const renderPhrase = (number) => {
+    //console.log(number);
+    if (number == 1 || number > 4) {
+      phrase = setPhrases(ph[0]);
+    }
+    if (number < 5 && number > 1) {
+      phrase = setPhrases(ph[1]);
+    }
+    if (number == 0) {
+      phrase = setPhrases(ph[2]);
+    }
   };
-  const nameUsers = users.map((user) => {
-    <td>{user.name}</td>;
-  });
+
+  const getBageClasses = (number) => {
+    //console.log(number);
+    let classes = number > 0 ? "badge text-bg-primary" : "badge text-bg-danger";
+    return classes;
+  };
 
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th scope="col">Имя</th>
-          <th scope="col">Качество</th>
-          <th scope="col">Професия</th>
-          <th scope="col">Встретился, раз</th>
-          <th scope="col">Оценка</th>
-        </tr>
-      </thead>
-      <tbody>
-        {users.map((name) => (
-          // <div>{name.name}</div>
+    <>
+      <div className={getBageClasses(number)}>
+        <h5>{`${number} ${phrase}`}</h5>
+      </div>
+
+      <table className="table">
+        <thead>
           <tr>
-            <td>{name.name}</td>
-            <td>{name.qualities.fetchAll()}</td>
-            <td>@{name.name}</td>
-            <td>@{name.name}</td>
-            <td>@{name.name}</td>
+            <th scope="col">Имя</th>
+            <th scope="col">Качество</th>
+            <th scope="col">Професия</th>
+            <th scope="col">Встретился, раз</th>
+            <th scope="col">Оценка</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+
+        <tbody>
+          {users.map((user) => (
+            <tr key={user._id}>
+              <td>{user.name}</td>
+              <td>
+                {user.qualities.map((qualiti) => {
+                  return (
+                    <div
+                      className={`badge bg-${qualiti.color}`}
+                      id="qualiti"
+                      key={qualiti._id}
+                    >
+                      {qualiti.name}
+                    </div>
+                  );
+                })}
+              </td>
+
+              <td>{user.profession.name}</td>
+              <td>{user.completedMeetings}</td>
+              <td>{user.rate}</td>
+              <td>
+                <button
+                  onClick={() => handleDelete(user._id)}
+                  className="btn btn-danger"
+                >
+                  delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
